99d6e1236ea40bc2948f66d50d0b0e81e23f2a0d
Mahmood <99671780+MahmoodWebDev@users.noreply.github.com>
2024-02-09 10:26:22 +0100

    on page load current date added

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
   const progressBar = document.getElementById("progress");
   const yearProgressText = document.getElementById("yearProgressText");
 
+  // Load current date by default
+  const currentDate = new Date();
+  dateInput.valueAsDate = currentDate;
+
+  // Calculate progress value based on the current date
+  const startOfYear = new Date(currentDate.getFullYear(), 0, 1);
+  const endOfYear = new Date(currentDate.getFullYear(), 11, 31);
+  const progressValue =
+    ((currentDate - startOfYear) / (endOfYear - startOfYear)) * 100;
+
+  // Set initial progress bar width and text content
+  progressBar.style.width = progressValue.toFixed(2) + "%";
+  yearProgressText.textContent = `2024 IS ${progressValue.toFixed(2)}% OVER`;
+
+  // Update progress bar and text content when the date input changes
   dateInput.addEventListener("change", function () {
     const selectedDate = new Date(this.value);
     const startOfYear = new Date(selectedDate.getFullYear(), 0, 1);
